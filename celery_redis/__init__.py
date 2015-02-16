f56640f0b79348ace313e09d3f250888d76c1062
christiancadieux <christian_cadieux@yahoo.com>
2015-02-15 17:39:03 -0700

    Update __init__.py
    fix all the issues I can think of. always, fast, takes no cpu, no risk of hanging forever.

--- celery_redis/__init__.py
+++ celery_redis/__init__.py
@@ -8,35 +8,44 @@ from celery.exceptions import TimeoutError
 
 
 class RedisBackend(_RedisBackend):
-    def wait_for(self, task_id,
-                 timeout=None, interval=0.5, no_ack=True, on_interval=None):
+    def wait_for(self, task_id,timeout=None, interval=0.5, no_ack=True, propagate=True):
+    
+        timeout = timeout or 600  # don't take any chance
+        def rread(key):
+          meta = self.client.get(key)
+          if meta:
+            meta = self.decode(meta)
+            if meta['status'] not in states.READY_STATES:
+              meta = None
+          return meta
+            
         key = self.get_key_for_task(task_id)
         p = self.client.pubsub(ignore_subscribe_messages=True)
         p.subscribe(key)
-        meta = self.client.get(key)
-        if meta:
-            meta = self.decode(meta)
-            if meta['status'] not in states.READY_STATES:
-                meta = None
+        meta = rread(key)
+
         if not meta:
-            if timeout:
-                time_elapsed = 0.0
-                while True:
-                    message = p.get_message()
-                    if message:
-                        break
-                    if on_interval:
-                        on_interval()
-                    # avoid hammering the CPU checking status.
-                    time.sleep(interval)
-                    time_elapsed += interval
-                    if time_elapsed >= timeout:
-                        raise TimeoutError('The operation timed out.')
-            else:
-                message = next(p.listen())
+            time_elapsed = 0.0 
+            while True:
+                start = time.time()
+                message = p.get_message(timeout=timeout)
+                interval2 = time.time() - start  #use the real interval
+                if message:
+                    break
+                
+                time_elapsed += interval2
+                if time_elapsed >= timeout:
+                    p.unsubscribe()
+                    p.close()
+                    meta = rread(key)
+                    if meta:
+                       return meta  # in case we missed the message
+                    raise TimeoutError('The operation timed out.')
+
             assert message['type'] == 'message'
             assert message['channel'] == key
             meta = self.decode(message['data'])
         p.unsubscribe()
         p.close()
         return meta
+
